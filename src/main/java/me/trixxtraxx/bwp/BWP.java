3a037cec91f4a620f9130f162a9163093bb4c9b1
TrixxTraxx <kosimo.weiss@gmail.com>
2022-03-03 10:06:04 +0100

    The Beginning

---
+++ src/main/java/me/trixxtraxx/bwp/BWP.java
@@ -0,0 +1,20 @@
+package me.trixxtraxx.bwp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BWP extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
