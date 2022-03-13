e0a10d5678d09be780e63cffe7aa9ae5171592c8
BuildTools <unconfigured@null.spigotmc.org>
2022-03-13 14:22:12 +0800

    Uhhh just testing if it works

--- src/main/java/me/trixxtraxx/Practice/GameLogic/SoloGameLogic/Components/Settings/SettingsComponent.java
+++ src/main/java/me/trixxtraxx/Practice/GameLogic/SoloGameLogic/Components/Settings/SettingsComponent.java
@@ -2,18 +2,33 @@ package me.trixxtraxx.Practice.GameLogic.SoloGameLogic.Components.Settings;
 
 import me.trixxtraxx.Practice.GameLogic.Components.GameComponent;
 import me.trixxtraxx.Practice.GameLogic.GameLogic;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SettingsComponent extends GameComponent
+public class SettingsComponent extends GameComponent implements ISettingsComponent
 {
     private Material mat;
     private List<StoredInventory> inv = new ArrayList<>();
+    private String title = ChatColor.AQUA + "Settings";
+
+    @Override
+    public int getSlot() {
+        return 0;
+    }
+
+    @Override
+    public ItemStack getItem() {
+        return null;
+    }
 
     private class StoredInventory
     {
@@ -24,7 +39,7 @@ public class SettingsComponent extends GameComponent
     public SettingsComponent(GameLogic logic, Material open)
     {
         super(logic);
-        mat = open;
+        this.mat = open;
     }
 
     @Override
@@ -35,9 +50,11 @@ public class SettingsComponent extends GameComponent
 
     public void onInteract(PlayerInteractEvent e)
     {
+        Player p = e.getPlayer();
         if(e.getItem() != null && e.getItem().getType() == mat)
         {
-            //open gui
+            Inventory setting = Bukkit.createInventory(p,53,title);
+            p.openInventory(setting);
         }
     }
 }
