b6da1c49122a763fa0fdb5ac8e361b1001316437
TrixxTraxx <kosimo.weiss@gmail.com>
2022-06-06 18:01:30 +0200

    CustomKbComponent

---
+++ src/main/java/me/trixxtraxx/Practice/GameLogic/Components/Components/CustomKbComponent.java
@@ -0,0 +1,122 @@
+package me.trixxtraxx.Practice.GameLogic.Components.Components;
+
+import me.trixxtraxx.Practice.GameLogic.Components.GameComponent;
+import me.trixxtraxx.Practice.GameLogic.GameLogic;
+import me.trixxtraxx.Practice.Practice;
+import me.trixxtraxx.Practice.TriggerEvent;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerToggleSprintEvent;
+import org.bukkit.event.player.PlayerVelocityEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class CustomKbComponent extends GameComponent
+{
+    public static double global = 1.0;
+    public static double horizontal = 1.0;
+    public static double air = 1.0;
+    public static double vertical = 0.5;
+    public static double sprint = 0.8;
+    public static double walk = 0.8;
+    
+    public CustomKbComponent(GameLogic logic)
+    {
+        super(logic);
+    }
+    
+    private HashMap<Player, Player> lastDamager = new HashMap<>();
+    private List<Player> sprintReset = new ArrayList<>();
+    private HashMap<Player, List<Long>> hitStack = new HashMap<>();
+    private List<Player> sprinting = new ArrayList<>();
+    
+    @TriggerEvent(state = TriggerEvent.CancelState.NONE)
+    public void onPlayerVelocity(EntityDamageByEntityEvent e)
+    {
+        if(e.getEntity() instanceof Player && e.getDamager() instanceof Player){
+            lastDamager.put((Player) e.getEntity(), (Player) e.getDamager());
+            new BukkitRunnable()
+            {
+                @Override
+                public void run()
+                {
+                    lastDamager.remove(e.getEntity());
+                }
+            }.runTaskLater(Practice.Instance, 0);
+        }
+    }
+    
+    @TriggerEvent(state = TriggerEvent.CancelState.ENSURE_NOT_CANCEL)
+    public void onPlayerMove(PlayerVelocityEvent e)
+    {
+        if(lastDamager.containsKey(e.getPlayer()))
+        {
+            Player damager = lastDamager.get(e.getPlayer());
+            Location dmg = lastDamager.get(e.getPlayer()).getLocation();
+            Player vik = e.getPlayer();
+            Location pl = e.getPlayer().getLocation();
+            
+            //calculate the vector from dmg to pl
+            Vector dir = pl.toVector().subtract(dmg.toVector());
+            
+            dir = dir.setY(0);
+            //normalize
+            dir = dir.normalize();
+            //multiple by horizontal
+            dir = dir.multiply(horizontal);
+            //multiple by ground if on ground
+            if(!vik.isOnGround())
+            {
+                dir = dir.multiply(air);
+            }
+            
+            if(sprinting.contains(damager))
+            {
+                if(sprintReset.contains(damager))
+                {
+                    dir = dir.multiply(sprint);
+                }
+                else
+                {
+                    sprintReset.add(damager);
+                }
+            }
+            else
+            {
+                dir = dir.multiply(walk);
+            }
+            
+            
+            //add vertical velocity
+            dir = dir.setY(vertical);
+            //multiply by global
+            dir = dir.multiply(global);
+            
+            //set the velocity of the player to the vector
+            e.setVelocity(dir);
+            
+            lastDamager.remove(e.getPlayer());
+        }
+    }
+    
+    @TriggerEvent(state = TriggerEvent.CancelState.ENSURE_NOT_CANCEL)
+    public void onSprintReset(PlayerToggleSprintEvent e)
+    {
+        if(!e.isSprinting())
+        {
+            sprintReset.remove(e.getPlayer());
+            sprinting.remove(e.getPlayer());
+        }
+        else
+        {
+            sprinting.add(e.getPlayer());
+        }
+    }
+}
